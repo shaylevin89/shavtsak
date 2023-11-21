67e0fbb4752142d7e0c823649e9b9c112379f860
shaylevin89 <shay.levin@perimeter81.com>
2023-11-21 23:31:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
         shuffle_names = st.checkbox("לערבב את סדר רשימת השמות")
         do_shuffle = st.checkbox("לערבב כל שורה בשבצק")
         # intervals = st.slider("?כמה שעות כל שמירה", 1, 8, 2)
-        intervals = st.number_input("?כמה שעות כל שמירה", value=float(2), min_value=0.25, step=0.25)
+        intervals = st.number_input("?כמה שעות כל שמירה", value=float(2), min_value=0.15, step=0.5)
         # positions_num = st.slider("?כמה עמדות יש", 1, 7, 3)
         positions_num = st.number_input("?כמה עמדות יש", value=3, min_value=1, step=1)
         st.session_state.shuffle_names = shuffle_names
